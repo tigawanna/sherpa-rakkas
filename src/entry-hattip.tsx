4bae53dff1bbed1265485d08d5425359291e1083
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2023-09-29 19:13:47 +0300

    hattip-entry theme persist

--- src/entry-hattip.tsx
+++ src/entry-hattip.tsx
@@ -1,10 +1,69 @@
 import { createRequestHandler } from "rakkasjs";
-
+import { cookie } from "@hattip/cookie";
 
 export default createRequestHandler({
-  // createPageHooks() {
-  //   return {
-  //     emitToDocumentHead: () => <ColorSchemeScript />,
-  //   };
-  // },
+  middleware: {
+    // HatTip middleware to be injected
+    // before the page routes handler.
+    beforePages: [cookie()],
+    // HatTip middleware to be injected
+    // after the page routes handler but
+    // before the API routes handler
+    beforeApiRoutes: [],
+    // HatTip middleware to be injected
+    // after the API routes handler but
+    // before the 404 handler
+    beforeNotFound: [],
+  },
+
+  createPageHooks(ctx) {
+    return {
+      emitBeforeSsrChunk() {
+        // Return a string to emit into React's
+        // SSR stream just before React emits a
+        // chunk of the page.
+        return "";
+      },
+
+      emitToDocumentHead() {
+        // Return a string or ReactElement to emit
+        // some HTML into the document's head.
+        const cookie_theme = ctx?.cookie?.theme;
+        return `
+      <script>
+      (function() {
+        document.documentElement.setAttribute("data-theme", "${cookie_theme}");
+      })();
+     </script>`;
+      },
+
+      extendPageContext(pageContext) {
+        // Add properties to the page context,
+        // especially to pageContext.locals.
+        // Extensions added here will only be
+        // available on the server-side.
+      },
+
+      wrapApp(app) {
+        // Wrap the Rakkas application in some provider
+        // component (only on the server).
+        // return <SomeProvider>{app}</SomeProvider>;
+        return app;
+      },
+
+      //   wrapSsrStream(stream) {
+      //     const { readable, writable } = new TransformStream({
+      //       transform(chunk, controller) {
+      //         // You can transform the chunks of the
+      //         // React SSR stream here.
+      //         controller.enqueue(chunk);
+      //       },
+      //     });
+      // // @ts-expect-error
+      //     stream.pipeThrough(writable);
+
+      //     return readable;
+      //   },
+    };
+  },
 });
